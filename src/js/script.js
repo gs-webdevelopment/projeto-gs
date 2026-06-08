861087154447acea7030d24b5b847d72de2689b2
yasmin <yasiunte@gmail.com>
2026-06-08 11:36:40 -0300

    feat: finalizando slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
+let imagens = [
+    "src/assets/img/slide1.png",
+    "src/assets/img/slide2.png",
+    "src/assets/img/slide3.png",
+];
+
+let index = 1;
+
+function trocarImagem(){
+    document.getElementById("banner").src = imagens[index];
+    index++;
+    if(index >= imagens.length){
+        index = 0;
+    }
+}
+
+setInterval(trocarImagem, 3000);
+
 const perguntas = [
     {
         pergunta: "Qual é o principal problema que o TAPS busca solucionar?",
